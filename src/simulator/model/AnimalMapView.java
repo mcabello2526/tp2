55ee43ac8245fd7ecbcf951a707328cf39e5bb77
mcabello2526 <mariacabellopastor04@gmail.com>
2026-02-18 19:41:27 +0100

    Add files via upload

---
+++ src/simulator/model/AnimalMapView.java
@@ -0,0 +1,10 @@
+package simulator.model;
+
+public interface AnimalMapView {
+
+	public int getWidth();
+	public int getHeight();
+	public double getFood(Animal a, double dt); 
+	 
+
+}
---
+++ src/simulator/model/RegionInfo.java
@@ -0,0 +1,6 @@
+package simulator.model;
+
+public interface RegionInfo {
+	// for now it is empty, later we will make it implement the interface  
+	// Iterable<AnimalInfo> 
+}
