97ab0f63ac90e83e330cee5f25a8ffed35be5d14
mcabello2526 <mariacabellopastor04@gmail.com>
2026-02-26 00:16:26 +0100

    Delete src directory

--- src/simulator/model/AnimalMapView.java
+++
@@ -1,8 +0,0 @@
-package simulator.model;
-
-import java.util.List;
-import java.util.function.Predicate;
-
-public interface AnimalMapView extends MapInfo, FoodSupplier {  
-	public List<Animal> getAnimalsInRange(Animal e, Predicate<Animal> filter);  
-}
