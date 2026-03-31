f002b6244269f05c77ac35dea072ae60b8c2873d
mcabello2526 <mariacabellopastor04@gmail.com>
2026-03-31 15:20:25 +0200

    Delete src directory

--- src/simulator/model/Entity.java
+++
@@ -1,7 +0,0 @@
-package simulator.model;
-
-
-// para actualizar los estados de los animales y las regiones en cada iteracion 
-public interface Entity {
-	public void update(double dt); 
-}
--- src/simulator/model/RegionInfo.java
+++
@@ -1,6 +0,0 @@
-package simulator.model;
-
-public interface RegionInfo extends JSONable {  
-	// for now it is empty, later we will make it implement the interface  
-	// Iterable<AnimalInfo>  
-}
