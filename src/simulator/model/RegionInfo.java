ae234853540e90b61ecb5e204c8b0edc32b469ac
mcabello2526 <mariacabellopastor04@gmail.com>
2026-03-31 15:21:50 +0200

    Add files via upload

---
+++ src/simulator/model/RegionInfo.java
@@ -0,0 +1,7 @@
+package simulator.model;
+
+public interface RegionInfo extends JSONable {
+	// for now it is empty, later we will make it implement the interface
+	// Iterable<AnimalInfo>
+	// es super de Collections<E>
+}
